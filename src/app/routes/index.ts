75b8ee33e5e4a3fa7bc8485fcaf84c1db7cb22e5
sabbir1054 <mdsabbir1054@gmail.com>
2023-10-14 21:52:46 +0600

    project starter file uploded

---
+++ src/app/routes/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const router = express.Router();
+
+const moduleRoutes = [
+  // ... routes
+ 
+  {
+    path: '/users',
+    route: "",
+  },
+  
+];
+
+// moduleRoutes.forEach(route => router.use(route.path, route.route));
+export default router;
